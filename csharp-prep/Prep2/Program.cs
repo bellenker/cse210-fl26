d548918f42daa15341776084f521bb9349e521fd
bellenker <libertybelle0524.er@gmail.com>
2026-09-16 15:07:15 -0600

    Most of my Prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your total grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine("You got a D.");
+        }
+        else if (grade > 0 && grade < 60)
+        {
+            Console.WriteLine("You got an F.");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, that is not a valid grade percentage. Try again.");
+        }
+        if (grade >= 70 && grade <= 100)
+        {
+            Console.WriteLine("Congratulations you pass!");
+        }
+        else if (grade >= 69)
+        {
+            Console.WriteLine("You are almost there! Try again, you can do it!");
+        }
+        else
+        {
+            Console.WriteLine("Try again and write your percentage as two numbers. Ex: 69");
+        }
     }
 }
